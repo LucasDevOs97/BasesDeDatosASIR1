df0716e3a29b5d17f02fdb918e90e4f085850f5d
naate20 <59051270+naate20@users.noreply.github.com>
2022-02-03 10:56:50 +0100

    Update programario.sql

--- proyecto1/programario.sql
+++ proyecto1/programario.sql
@@ -1,21 +1,15 @@
-INSERT INTO programa (nombre, version) VALUES
-("Application Server", "9i"),
-("Oracle Database", "8i"),
-("Database", "91"),
-("Database", "10g"),
-("Developer", "6i"),
-("Access", "97"),
-("Access", "2000"),
-("Access", "XP"),
-("Windows", "98"),
-("Windows", "XP Professional"),
-("Windows", "XP Home Edition"),
-("Windows", "2003 Server"),
-("Norton Internet Security", "2004"),
-("Freddy Hardest", NULL),
-("Paradox", "2"),
-("C++ Builder", "5.5"),
-("DB/2", "2.0"),
-("OS/2", "1.0"),
-("JBuilder", "X"),
-("La Prision", "1.0");
+-- 1) Averigua el DNI de todos los clientes. --
+
+
+
+-- 2) Consulta todos los datos de todos los programas. --
+-- 3) Obtén un listado con los nombres de todos los programas. --
+-- 4) Genera una lista con todos los comercios. --
+/* 5) Genera una lista de las ciudades con establecimientos donde se venden programas, sin que aparezcan 
+valores duplicados (utiliza DISTINCT).*/
+-- 6) Obtén una lista con los nombres de programas, sin que aparezcan valores duplicados (utiliza DISTINCT). --
+-- 7) Obtén el DNI más 4 de todos los clientes. --
+-- 8) Haz un listado con los códigos de los programas multiplicados por 7. --
+-- 9) ¿Cuáles son los programas cuyo código es inferior o igual a 10? --
+-- 10) ¿Cuál es el programa cuyo código es 11? --
+-- 11) ¿Qué fabricantes son de Estados Unidos? --
