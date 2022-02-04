2dd650221ffc50d4306f14e40aeb9d9b5d345e82
naate20 <59051270+naate20@users.noreply.github.com>
2022-02-04 13:01:38 +0100

    Update programario.sql

--- proyecto1/programario.sql
+++ proyecto1/programario.sql
@@ -25,6 +25,10 @@ SELECT DISTINCT comercio.ciudad
 FROM comercio INNER JOIN distribuye
     ON comercio.cif = distribuye.cif;
 
+-- Mejor esta forma --
+
+SELECT DISTINCT ciudad
+FROM comercio;
 
 -- 6) Obtén una lista con los nombres de programas, sin que aparezcan valores duplicados (utiliza DISTINCT). --
 
@@ -96,6 +100,10 @@ SELECT nombre
 FROM cliente
 WHERE edad BETWEEN 10 AND 25 OR edad > 50;
 
+SELECT nombre
+FROM cliente
+WHERE (edad > 9 AND edad < 26) OR edad > 50;
+
 /*18. Saca un listado con los comercios de Sevilla y Madrid. No se admiten valores duplicados.*/
 
 SELECT DISTINCT nombre
@@ -130,7 +138,7 @@ WHERE (version LIKE("%i")) OR (nombre LIKE("A%s"));
 
 SELECT *
 FROM programa 
-WHERE version LIKE("%i") AND nombre LIKE("A%");
+WHERE version LIKE("%i") AND nombre NOT LIKE("A%");
 
 -- 24. Obtén una lista de empresas por orden alfabético ascendente. --
 
@@ -159,30 +167,115 @@ FROM programa INNER JOIN desarrolla
     fabricante.id_fab = desarrolla.id_fab
 WHERE fabricante.nombre = "Oracle";
 
+-- Uso de USING (solo cuando unimos dos columnas cuyos ids, FK y PK, tienen el mismo nombre) --
+
+SELECT progrma.*
+FROM programa INNER JOIN desarrolla
+    USING (codigo)
+    INNER JOIN fabricante 
+    USING (id_fab)
+WHERE fabricante.nombre = "Oracle";
+
 -- 28) ¿Qué comercios distribuyen Windows? --
 
+SELECT comercio*
+FROM comercio INNER JOIN distribuye
+    ON comercio.cif = distribuye.cif
+    INNER JOIN programa
+    ON distribuye.codigo = programa.codigo
+WHERE programa.nombre = "Windows";
+
+-- Con USING --
+
+SELECT comercio.*
+FROM comercio INNER JOIN distribuye
+    USING (cif)
+    INNER JOIN programa
+    USING (codigo)
+WHERE programa.nombre = "Windows";
 
 -- 29. Genera un listado de los programas y cantidades que se han distribuido a El  Corte Inglés de Madrid. --
 
+SELECT COUNT(programa.codigo) AS "Programas distribuidos a El Corte Inglés de Madrid"
+FROM programa INNER JOIN distribuye
+    ON programa.codigo = distribuye.codigo
+    INNER JOIN comercio
+    ON distribuye.cif = comercio.cif
+WHERE comercio.nombre = "El Corte Inglés" AND comercio.ciudad = "Madrid"
+GROUP BY nombre;
+
 -- 30. ¿Qué fabricante ha desarrollado Freddy Hardest? --
+
+SELECT fabricante.nombre
+FROM fabricante INNER JOIN desarrolla
+    ON fabricante.id_fab = desarrolla.id_fab
+    INNER JOIN programa
+    ON desarrolla.codigo = programa.codigo
+WHERE programa.nombre = "Freddy Hardest";
 
 -- 31. ) Selecciona el nombre de los programas que se registran por Internet. --
 
+SELECT programa.nombre
+FROM programa INNER JOIN registra
+    ON programa.codigo = registra.codigo
+WHERE registra.medio = "Internet";
+
 -- 32. ) Selecciona el nombre de las personas que se registran por Internet. --
+
+SELECT cliente.nombre
+FROM cliente INNER JOIN registra
+    ON cliente.dni = registra.dni
+WHERE registra.medio = "Internet";
 
 -- 33) ¿Qué medios ha utilizado para registrarse Pepe Pérez? --
 
+SELECT registra.medio
+FROM cliente INNER JOIN registra
+    ON cliente.dni = registra.dni
+WHERE cliente.nombre = "Pepe Pérez";
+
 -- 34. ) ¿Qué usuarios han optado por Internet como medio de registro? --
+
+SELECT cliente.nombre
+FROM cliente INNER JOIN registra
+    ON cliente.dni = registra.dni
+WHERE registra.medio = "Internet";
+
+
+
+
+-- LA ÚLTIMA QUE CORREGIMOS FUE LA 34 --
+
 
 -- 35. ) ¿Qué programas han recibido registros por tarjeta postal? --
 
+SELECT programa.nombre
+FROM programa INNER JOIN registra
+    ON programa.codigo = registra.codigo
+WHERE registra.medio = "Tarjeta postal";
+
 -- 36. ) ¿En qué localidades se han vendido productos que se han registrado por Internet? --
+
+SELECT comercio.ciudad
+FROM comercio INNER JOIN registra
+    ON comercio.cif = registra.cif
+WHERE registra.medio = "Internet";
 
 /*37. ) Obtén un listado de los nombres de las personas que se han registrado por Internet, junto al nombre de los programas para los 
 que ha efectuado el registro. */
 
+SELECT cliente.nombre, programa.nombre
+FROM programa INNER JOIN registra
+    ON programa.codigo = registra.codigo
+    INNER JOIN cliente
+    ON registra.dni = cliente.dni
+WHERE registra.medio = "Internet";
+
 /*38. ) Genera un listado en el que aparezca cada cliente junto al programa que ha registrado, el medio con el que lo ha hecho y el 
 comercio en el que lo ha adquirido.*/
+
+SELECT cliente.*, programa.nombre, registra.medio, comercio.nombre
+FROM 
 
 -- 39.Obtén el nombre de los usuarios que han registrado el programa "Paradox" en su versión "2". --
 
