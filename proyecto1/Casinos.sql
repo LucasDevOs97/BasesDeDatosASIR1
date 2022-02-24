d7462e194629716351080f7337b5a836c6fa70dd
naate20 <59051270+naate20@users.noreply.github.com>
2022-02-24 13:40:26 +0100

    Finalización ejercicio del casino, examen DAM

--- proyecto1/Casinos.sql
+++ proyecto1/Casinos.sql
@@ -21,12 +21,67 @@ WHERE torneosjugadores.idJugador IS NULL;
 /* 3. Muestra el nombre del jugador, el nombre del torneo y el nombre de la modalidad favorita del jugador que ha jugado un 
 torneo con un premio mayor a la media de todos los torneos de nuestra base de datos.*/
 
-
+SELECT jugadores.nombre, torneos.nombre, modalidad.nombre
+FROM torneos INNER JOIN torneosjugadores
+    ON torneos.id = torneosjugadores.idTorneo
+    INNER JOIN jugadores
+    ON torneosjugadores.idJugador = jugadores.id
+    INNER JOIN modalidad
+    ON jugadores.idModalidad = modalidad.id
+WHERE torneos.premio > (
+    SELECT AVG(premio)
+    FROM torneos
+);
 
 /* 4. Muestra la cantidad de caracteres que tiene el nombre del jugador que tiene como modalidad favorita Omaha y que ha 
 jugado en Barcelona o de aquel jugador que participase en un torneo con premio mayor a 100 000.*/
+
+SELECT CHAR_LENGTH(jugadores.nombre) AS "Longitud del nombre del jugador"
+FROM modalidad INNER JOIN jugadores 
+    ON modalidad.idJugador = jugadores.id
+    INNER JOIN torneosjugadores
+    ON jugadores.id = torneosjugadores.idJugador
+    INNER JOIN torneos
+    ON torneosjugadores.idTorneo = torneos.id
+    INNER JOIN casino
+    ON torneos.idCasino = casino.id
+WHERE (jugadores.modalidad = (
+    SELECT modalidad.id
+    FROM modalidad
+    WHERE modalidad.nombre LIKE "Omaha"
+) AND casino.localidad = "Barcelona") OR torneos.premio > 100000.00;
+
 -- 5. Elimina los torneos que no tengan ningún participante. --
+
+DELETE
+FROM torneos 
+WHERE id IN (
+    SELECT torneos.id
+    FROM torneos LEFT JOIN torneosjugadores
+        ON torneos.id = torneosjugadores.idTorneo
+    WHERE torneosjugadores.idJugador IS NULL
+);
+
 -- 6. Muestra la información de aquellos jugadores que hayan participado en más de 1 torneo. --
+
+SELECT jugadores.*
+FROM jugadores INNER JOIN torneosjugadores
+    ON jugadores.id = torneosjugadores.idJugador
+GROUP BY torneosjugadores.idJugador
+HAVING COUNT(DISTINCT torneosjugadores.idTorneo) > 1;
+
 /* 7. Muestra la información de los jugadores que hayan jugado 3 torneos y que tengan como segunda letra del nombre una “e” y 
 como última una “s”. */
+
+SELECT jugadores.*
+FROM jugadores INNER JOIN torneosjugadores
+    ON jugadores.id = torneosjugadores.idJugador
+WHERE jugadores.nombre LIKE "_e%s" AND
+HAVING COUNT(DISTINCT torneosjugadores.idTorneo) = 3;
+
 -- 8. Muestra cuánto suman los premios de los torneos según la localidad en la que se celebren. --
+
+SELECT SUM(torneos.premio) AS "Suma de los premios", casino.localidad
+FROM torneos INNER JOIN casino  
+    ON torneos.idCasino = casino.id
+GROUP BY casino.localidad;
